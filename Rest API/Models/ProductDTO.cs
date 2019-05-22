8033c67e073aef964bc14262b07ed54598fa3a28
dwebdevcore <dwebdevcore@gmail.com>
2019-05-22 18:48:12 -0400

    Database and Restful api

---
+++ Rest API/Models/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace shopdevAPI.Models
+{
+    public class ProductDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string DisplayImage { get; set; }
+    }
+}
